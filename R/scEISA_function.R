92904f2882e8e352fc32454112b1eafcb8591eca
Jean Wen <jiayu.jean.wen@gmail.com>
2019-11-11 16:45:00 +1100

    modified to make contrast matrix as input

--- R/scEISA_function.R
+++ R/scEISA_function.R
@@ -1,10 +1,7 @@
-library(dplyr)
-library(limma)
-library(edgeR)
-
-
 # THE edgeR package
-run_edgeRQLF <- function(L) {
+run_edgeRQLF_withCont <- function(L) {
+  library(limma)
+  library(edgeR)
   message("edgeRQLF")
   session_info <- sessionInfo()
   timing <- system.time({
@@ -37,15 +34,27 @@ run_edgeRQLF <- function(L) {
 scEISA <- function(exon_list, intron_list) {
   message("scEISA")
   session_info <- sessionInfo()
+  library(dplyr)
   # the exon_list and intron list should contain condition and count matrix
-  conditions = c(exon_list$condt, intron_list$condt) #combine the conditions
+  condit = c(exon_list$condt, intron_list$condt) #combine the conditions
+  treatment <- unique(L$condt)
+  {if (length(treatment) !=2)
+    + stop ('the function only takes 2 treatments')
+    }
+  len = length(conditions)/4
+  #len for condition 1 must equal condition 2
+  conditions = c(
+    rep('cont1', len),
+    c(rep('cont2', len)),
+    rep('cont1', len),
+    c(rep('cont2', len)))
   cnt <- cbind(Ex=as.data.frame(cntEx[genes.sel,]), In=cntIn[genes.sel,])
   genes_no_overlap <- rownames(cntEx)
   genes.sel <-genes_no_overlap
   cntEx = exon_list$count
   cntIn = intron_list$count
   cnt = cbind(Ex=as.data.frame(cntEx[genes.sel,]), In=cntIn[genes.sel,])
-  dge = DGEList(counts=cnt, genes=data.frame(ENTREZID=rownames(cnt)))
+  dge = DGEList(counts = cnt, genes = data.frame(ENTREZID=rownames(cnt)))
   dge = calcNormFactors(dge)
   libsize_sel = data.frame(lib=colnames(cnt)) %>%
     mutate(factorRegion=factor(rep(c("ex","in"),each=ncol(cntEx)), levels=c("in", "ex"))) %>%
@@ -55,11 +64,11 @@ scEISA <- function(exon_list, intron_list) {
   design <- model.matrix(~0+Treat)
   colnames(design) <- levels(Treat)
   cont.matrix = cont.matrix <- makeContrasts(
-    in_mc = in_cont1 - in_cont2,
-    ex_mc = ex_cont1 - ex_cont2,
-    exonMin_mc = (ex_cont1 - in_cont1) - (ex_cont2- in_cont2),
+    in_cont1_cont2 = in_cont1 - in_cont2,
+    ex_cont1_cont2 = ex_cont1 - ex_cont2,
+    exonMin_intron = (ex_cont1 - in_cont1) - (ex_cont2- in_cont2),
     levels=design)  # build contrast matrix
   L <- list(count = cnt, condt = conditions, cont.matrix = cont.matrix, Treat = Treat)
-  run_edgeRQLF(L)
+  run_edgeRQLF_withCont(L)
 }
 
